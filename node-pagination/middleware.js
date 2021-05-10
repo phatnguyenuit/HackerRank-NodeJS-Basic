6ee18ec2adc324b7d5f3fdaef50a13e0dcc1a2c3
Nguyen Phat <phatnt.uit@gmail.com>
2021-05-10 22:07:38 +0700

    feat: add hackerrank nodejs basic test

---
+++ node-pagination/middleware.js
@@ -0,0 +1,13 @@
+module.exports = (req, res, next) => {
+  const { page = 1, limit = 3, q = '' } = req.query;
+
+  console.log("query", q);
+  req.context = {
+    page: +page,
+    limit: +limit,
+    skip: (page - 1) * limit,
+    searchTerm: q,
+    search: new RegExp(q, "gi")
+  };
+  next();
+};
